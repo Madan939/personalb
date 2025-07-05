f26001abe6784f7ed7220df87d7c25b572cb8ead
Madan Golay Tamang <madantamang939@gmail.com>
2025-07-05 11:32:11 +0545

    new update

--- index.js
+++ index.js
@@ -62,9 +62,9 @@ async function getAllMp4Files(service, creatorId) {
   return mp4Files;
 }
 
-app.get("/creator/lauren_demarcus/mp4videos", async (req, res) => {
+app.get("/creator/kirawrrra/mp4videos", async (req, res) => {
   try {
-    const creatorName = "lauren_demarcus";
+    const creatorName = "kirawrrra";
     const creator = await findCreatorByName(creatorName);
 
     if (!creator) return res.status(404).json({ error: "Creator not found" });
